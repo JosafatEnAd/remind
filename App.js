eaadad932b31e4a7689307805e5faf575a882647
Adolfo Salomé Hernández <98220340+SalomeAdolfo@users.noreply.github.com>
2022-11-24 22:33:24 -0600

    Speech the text

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const [name, setName] = React.useState('')
+  const speak = () => {
+    const thingToSay = `Hola ${name}`;
+    const options = {
+      voice: "com.apple.speech.synthesis.voice.Fred",
+      pitch: .9,
+      rate: 1
+    }
+    Speech.speak(thingToSay, options);
+  };
+  console.log(name)
   return (
-    <View style={styles.container}>
-      <Text>No</Text>
-      <StatusBar style="auto" />
+    <View>
+      <TextInput placeholder='Nombre' onChangeText={setName} />
+      <Button title="Press to hear some words" onPress={speak} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
