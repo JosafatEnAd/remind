4c425a9ae21ef4e27ce429e4f2adebf09bbbde93
Josafat <al222011221@gmail.com>
2022-12-03 00:34:06 -0600

    Modificación del diseño de la aplicación, agregado de linear gradient mediante expo

--- src/components/HaderComponent.js
+++ src/components/HaderComponent.js
@@ -1,32 +1,56 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-const HaderComponent = () => {
+const HaderComponent=() => {
     return (
-        <View style={[styles.HeaderContainer, { backgroundColor: '#fff' }]}>
+        <View style={[styles.HeaderContainer, { backgroundColor: '#FFFFFF' }]}>
             <View>
                 <Text style={[styles.LogoText]}>M</Text>
+            </View>
+            <View style={[styles.TextHeaderBox]}>
+                <Text style={[styles.TextHeader]}>MEDICINE</Text>
+                <Text style={[styles.TextHeader]}>REMINDER</Text>
             </View>
         </View>
     )
 }
-const styles = StyleSheet.create({
+const styles=StyleSheet.create({
     HeaderContainer: {
-        alignSelf: 'center',
-        justifyContent: 'center',
-        alignItems: 'stretch',
-        width: '95%'
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        height: 125,
+        width: 'auto',
+    },
+    TextHeader: {
+        color: '#121212',
+        fontSize: 50,
+        textShadowColor: '#2e2e2e',
+        textShadowRadius: 15,
+        textShadowOffset: {
+            width: 4,
+            height: 4
+        }
+    },
+    TextHeaderBox: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: 'auto',
+        height: 55
     },
     LogoText: {
-        color: '#D989B5',
-        textShadowColor: '#863A6F',
+        textAlignVertical: 'top',
+        height: '70%',
+        width: '100%',
+        color: '#1a79ff',
+        textShadowColor: 'blue',
         textShadowOffset: {
-            width: 6,
-            height: 8
+            width: 5,
+            height: 5
         },
-        textShadowRadius: 28,
-        fontSize: 100,
-        fontWeight: '300'
+        textShadowRadius: 15,
+        fontSize: 80,
+        fontWeight: '700'
     },
 })
 export default HaderComponent
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,34 +1,108 @@
-import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { ScrollView, SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
 import HaderComponent from '../components/HaderComponent'
 import IndexComponent from '../components/IndexComponent'
 import AddComponent from '../components/AddComponent'
 
-const MainScreen = () => {
+const MainScreen=() => {
     return (
-        <>
-            <View style={styles.headerContainer}>
+        <SafeAreaView >
+            <LinearGradient colors={['rgba(240, 240, 255, 1)', 'rgba(53, 175, 227, 1)', 'rgba(49, 150, 227, 1))', 'rgba(29, 89, 211, 1)', 'rgba(20, 63, 148, 1)']} style={[styles.MainContainer]}>
+                {/* Invocación del componente para el header */}
                 <HaderComponent />
-            </View>
-            <View style={styles.container}>
-                <IndexComponent />
-            </View>
-
-            <View style={styles.button_add}>
-                <AddComponent />
-            </View>
-        </>
+                <View style={[styles.Body, { justifyContent: 'flex-start' }]}>
+                    <View style={[styles.VoiceBox, { alignSelf: 'center' }]}>
+                        <Text style={[styles.TextVoiceBox]}>Toca para usar el microfono</Text>
+                        <Image source={require('../resources/microfono_icono.png')} style={[{ width: 45, height: 45, marginTop: 2 }]} />
+                    </View>
+                    <Text style={[{ fontSize: 20, fontWeight: '900', alignSelf: 'center', marginTop: 25 }]}>Todo lo que tienes que tomar hoy:</Text>
+                    <View style={[styles.BodyRContainer]}>
+                        <View style={[styles.BodyCContainer]}>
+                            <IndexComponent />
+                        </View>
+                    </View>
+                    <View style={[{ marginLeft: 350, height: 30, width: 30 }]}>
+                        <AddComponent />
+                    </View>
+                </View>
+            </LinearGradient>
+        </SafeAreaView>
     )
 }
-const styles = StyleSheet.create({
-    headerContainer: {
-        flex: .51,
+const styles=StyleSheet.create({
+    MainContainer: {
+        width: '100%',
+        height: '100%'
     },
-    container: {
-        flex: 2,
+    Body: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
     },
-    button_add: {
-        height: 100,
+    VoiceBox: {
+        justifyContent: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        marginTop: 30,
+        height: 50,
+        width: 350,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 20,
+        borderColor: 'black',
+        borderWidth: 2
+    },
+    TextVoiceBox: {
+        marginLeft: 10,
+        height: '100%',
+        width: '75%',
+        color: 'black',
+        fontSize: 15,
+        textAlignVertical: 'center',
+        textAlign: 'center'
+    },
+    BodyRContainer: {
+        width: '100%',
+        height: 500,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    BodyCContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly'
+    },
+    BodyContainer: {
+        backgroundColor: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        width: 150,
+        height: 150
+    },
+    BodyContainerText: {
+        fontSize: 15,
+        color: 'black',
+        textAlign: 'center',
+        fontWeight: '500'
+    },
+    BotonRecordatorio: {
+        justifyContent: 'space-around',
+        display: 'flex',
+        flexDirection: 'row',
+        width: '75%',
+        height: 65,
+        backgroundColor: 'white',
+        alignSelf: 'center',
+        borderRadius: 75
+    },
+    BotonText: {
+        fontSize: 25,
+        color: 'black',
+        height: '100%',
+        textAlignVertical: 'center'
     }
 })
 export default MainScreen
